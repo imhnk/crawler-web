03b08d837c214e07ff9f5da131291fb7708ded37
imhnk <victor.nguyen7491@gmail.com>
2024-08-06 13:29:32 +0700

    update to runner cd

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker rm -f website &2>/dev/null || true
+docker images --format '{{.Repository}}:{{.Tag}}' | grep 'hero2510/crawler-website' | xargs -r docker rmi
+docker run --net crawler --name website -p 3100:80 $1
