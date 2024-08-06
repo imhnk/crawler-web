69dc00de0ef9c5bfc81a6c90d357a4240585ed93
imhnk <victor.nguyen7491@gmail.com>
2024-08-06 23:13:10 +0700

    fix(runner): to run cd

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker rm -f website &2>/dev/null || true
 docker images --format '{{.Repository}}:{{.Tag}}' | grep 'imhnk/crawler-website-lab' | xargs -r docker rmi -f
-docker run --net crawler --name website -p 3100:80 $1
+docker run --net crawler -d --name website -p 3100:80 $1
